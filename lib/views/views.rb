111bb18b72490efa22e99696820bb9ae36bdd7d2
mhansbury <mhansbury@mitre.org>
2017-05-18 14:26:41 -0400

    Fixed formatting a bit

--- lib/views/views.rb
+++ lib/views/views.rb
@@ -1,9 +1,10 @@
-#VIEWS WS gem
-require "views/version"
+# VIEWS WS gem
+require 'views/version'
 require 'nokogiri'
 require 'savon'
 require 'logger'
 
+# the Views module
 module Views
   class << self
     attr_accessor :configuration
@@ -14,10 +15,11 @@ module Views
     yield(configuration)
   end
 
+  # Configuration for the class
   class Configuration
     attr_accessor :wsdl, :ssl_verify_mode, :soap_version, :log
     attr_accessor :logger
-    
+
     def initialize
       @wsdl = 'https://wwwn.cdc.gov/view2_admin/VIEWS/ValidationService.svc?wsdl'
       @ssl_verify_mode = :none
@@ -28,50 +30,48 @@ module Views
     end
   end
 
-  def self.validate(cause_of_death_line1: '', 
-      cause_of_death_duration1: '',
-      cause_of_death_line2: '', 
-      cause_of_death_duration2: '',
-      cause_of_death_line3: '', 
-      cause_of_death_duration3: '',
-      cause_of_death_line4: '', 
-      cause_of_death_duration4: '',
-      actual_or_presumed_date_of_death: nil,
-      date_of_injury: nil,
-      time_of_injury: nil,
-      place_of_injury: '', 
-      description_of_injury_occurrence: '',
-      transportation_injury_role: '',
-      sex: '',
-      date_of_birth: nil,
-      did_tobacco_use_contribute_to_death: nil,
-      was_an_autopsy_performed: nil,
-      were_autopsy_findings_available: nil,
-      manner_of_death: '',
-      injury_at_work: '')
+  def self.validate(cause_of_death_line1: '',
+                    cause_of_death_duration1: '',
+                    cause_of_death_line2: '',
+                    cause_of_death_duration2: '',
+                    cause_of_death_line3: '',
+                    cause_of_death_duration3: '',
+                    cause_of_death_line4: '',
+                    cause_of_death_duration4: '',
+                    actual_or_presumed_date_of_death: nil,
+                    date_of_injury: nil,
+                    time_of_injury: nil,
+                    place_of_injury: '',
+                    description_of_injury_occurrence: '',
+                    transportation_injury_role: '',
+                    sex: '',
+                    date_of_birth: nil,
+                    did_tobacco_use_contribute_to_death: nil,
+                    was_an_autopsy_performed: nil,
+                    were_autopsy_findings_available: nil,
+                    manner_of_death: '',
+                    injury_at_work: '')
 
     # initialize Savon (SOAP WS) client
-    client = Savon.client(
-      wsdl: Views.configuration.wsdl,
-      ssl_verify_mode: configuration.ssl_verify_mode,
-      soap_version: configuration.soap_version,
-      headers: { "wsa:To" => "https://wwwn.cdc.gov/view2_admin/VIEWS/ValidationService.svc"},
-      use_wsa_headers: true,
-      pretty_print_xml: true,
-      log: configuration.log
-    )
-  
-    # create array of values to create 
-    
+    client = Savon.client(wsdl: Views.configuration.wsdl,
+                          ssl_verify_mode: configuration.ssl_verify_mode,
+                          soap_version: configuration.soap_version,
+                          headers: { 'wsa:To' => 'https://wwwn.cdc.gov/view2_admin/VIEWS/ValidationService.svc' },
+                          use_wsa_headers: true,
+                          pretty_print_xml: true,
+                          log: configuration.log)
+
+    # create array of values to create
+
     # build the xml message
     # TODO: this should be done by the Savon client, had issues getting it working, fix
     message = "<input>
 &lt;Certificate Year='2000' State='NC' ID='012345' xmlns='WebMMDS'
-ValidateAbbreviations=\"Y\" ABBRStrict=\"1\" 
-ValidateMedicalEdits=\"Y\" MEDEDITStrict=\"1\" 
-ValidateRareCauses=\"Y\" RARECAUSEStrict=\"1\" 
-ValidateIllTrivial=\"Y\" ValidateSpelling=\"Y\" 
-ValidateSurveillance=\"Y\" ValidateMannerOfDeath=\"Y\" 
+ValidateAbbreviations=\"Y\" ABBRStrict=\"1\"
+ValidateMedicalEdits=\"Y\" MEDEDITStrict=\"1\"
+ValidateRareCauses=\"Y\" RARECAUSEStrict=\"1\"
+ValidateIllTrivial=\"Y\" ValidateSpelling=\"Y\"
+ValidateSurveillance=\"Y\" ValidateMannerOfDeath=\"Y\"
 TermCasing=\"UPPER\"&gt;\n"
 
     add(message, 'Line1a', cause_of_death_line1)
@@ -84,68 +84,68 @@ TermCasing=\"UPPER\"&gt;\n"
     add(message, 'Duration1c', cause_of_death_duration3)
     add(message, 'Duration1d', cause_of_death_duration4)
     add(message, 'YearOfDeath', parse_date(actual_or_presumed_date_of_death, :year))
-    add(message, 'MonthOfDeath', parse_date(actual_or_presumed_date_of_death, :month)) 
+    add(message, 'MonthOfDeath', parse_date(actual_or_presumed_date_of_death, :month))
     add(message, 'DayOfDeath', parse_date(actual_or_presumed_date_of_death, :day))
     # TODO: Month, Day, Year of Surgery can be added here
     add(message, 'YearOfInjury', parse_date(date_of_injury, :year))
-    add(message, 'MonthOfInjury', parse_date(date_of_injury, :month)) 
+    add(message, 'MonthOfInjury', parse_date(date_of_injury, :month))
     add(message, 'DayOfInjury', parse_date(date_of_injury, :day))
     add(message, 'HourOfInjury', parse_hour(time_of_injury))
     # InjuryTimeCode indicates the format for the HourOfInjury 'M' = military time
-    add(message, 'InjuryTimeCode', 'M') if (time_of_injury)
+    add(message, 'InjuryTimeCode', 'M') if time_of_injury
     add(message, 'PlaceOfInjury', place_of_injury)
     add(message, 'InjuryDescription', description_of_injury_occurrence)
     add(message, 'Transport', transportation_injury_role)
     add(message, 'Sex', sex)
-    add(message, 'AgeUnits', '1')  # ?? totally unclear what the allowed values mean here, guessing 1 is 'years'
+    add(message, 'AgeUnits', '1') # ?? totally unclear what the allowed values mean here, guessing 1 is 'years'
     add(message, 'Age', calculate_age(date_of_birth))
     add(message, 'TobaccoUse', translate_tobacco(did_tobacco_use_contribute_to_death))
     add(message, 'Autopsy', translate_yes_no_unknown(was_an_autopsy_performed))
     add(message, 'AutopsyFindings', translate_yes_no_unknown(were_autopsy_findings_available))
     # pregnancy is coded using numbers, with no idea what they mean, skipping for now
     # TODO:  figure this out and add
-    #add('Pregnancy', translatePregnancyStatus(pregnancy_status)  
+    # add('Pregnancy', translatePregnancyStatus(pregnancy_status)
     add(message, 'MannerOfDeath', translate_manner_of_death(manner_of_death))
-    add(message, 'WorkInjury', translate_yes_no_unknown(injury_at_work)) 
+    add(message, 'WorkInjury', translate_yes_no_unknown(injury_at_work))
     # activity code is also some kind of number without documentation
     # TODO:  Add if needed
 
-    #@logger.debug("message so far: " + message)
-    
+    # @logger.debug("message so far: " + message)
+
     message << '&lt;/Certificate&gt;
 </input>'
-    
-    # NOTE:  The 'attributes' bit below is required to get this to work properly.  
+
+    # NOTE:  The 'attributes' bit below is required to get this to work properly.
     # otherwise the 'Validate' tag isn't correctly created in the SOAP body
-    response = client.call(:validate, message: message, 
-                                      attributes: {'xmlns:ns2' => 'http://schemas.microsoft.com/2003/10/Serialization/', 
-                                                   'xmlns' => 'http://tempuri.org/'})
-      
-    Views.configuration.logger.debug("full savon response: " + response&.to_s)
+    response = client.call(:validate, message: message,
+                                      attributes: { 'xmlns:ns2' => 'http://schemas.microsoft.com/2003/10/Serialization/',
+                                                    'xmlns' => 'http://tempuri.org/' })
+
+    Views.configuration.logger.debug('full savon response: ' + response&.to_s)
     parse_response(response.body[:validate_response][:validate_result].to_s)
   end
 end
 
-private 
+private
 
 def add(message, tag, value)
-  if (value) 
+  if value
     Views.configuration.logger.debug(tag + ': ' + value.to_s)
-    message << "&lt;" << tag << "&gt;" << value << "&lt;/" << tag << "&gt;\n" if (value && value != '') 
+    message << '&lt;' << tag << '&gt;' << value << '&lt;/' << tag << "&gt;\n" if value && value != ''
   end
 end
 
 def parse_date(d, type)
-  if (d)
+  if d
     case type
-    when :year 
+    when :year
       response = d&.year&.to_s
     when :month
       response = d&.month&.to_s
-      response = response.rjust(2, '0') if (response)
+      response = response.rjust(2, '0') if response
     when :day
       response = d&.day&.to_s
-      response = response.rjust(2, '0') if (response)
+      response = response.rjust(2, '0') if response
     end
     response
   end
@@ -159,17 +159,17 @@ def parse_hour(time)
 end
 
 def calculate_age(dob)
-  if (dob)
+  if dob
     now = Time.now.utc.to_date
     response = now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
     # age must be no more than 130
     response = 130 if response > 130
-    response = response.to_s.rjust(3, '0')
+    response.to_s.rjust(3, '0')
   end
 end
 
-def translate_tobacco(tobaccoUse) 
-  case tobaccoUse
+def translate_tobacco(tobacco_use)
+  case tobacco_use
   when 'Yes'
     'Y'
   when 'No'
@@ -209,66 +209,65 @@ def translate_manner_of_death(val)
   end
 end
 
-def parse_response(response) 
+def parse_response(response)
   Views.configuration.logger.debug('response' + response&.to_s)
   return '' unless response
   doc = Nokogiri::XML(response)
   messages = {}
-  begin 
+  begin
     list = doc.xpath('//WebMMDS:ValidationData', WebMMDS: 'WebMMDS')
-    Views.configuration.logger.info("got list: " + list&.to_s)
+    Views.configuration.logger.info('got list: ' + list&.to_s)
     list&.each_with_index do |message, i|
       type = message.at_xpath('@type')
-      Views.configuration.logger.info("type: " + type&.to_s)
+      Views.configuration.logger.info('type: ' + type&.to_s)
       field = message.at_xpath('@field')
-      Views.configuration.logger.info("field " + field&.to_s)
+      Views.configuration.logger.info('field ' + field&.to_s)
       term = message.at_xpath('./WebMMDS:Term', WebMMDS: 'WebMMDS')
-      Views.configuration.logger.info("term: " + term&.text)
+      Views.configuration.logger.info('term: ' + term&.text)
       case type&.inner_text
       when 'Information'
         msg = message.at_xpath('./WebMMDS:Message[@level=3]', WebMMDS: 'WebMMDS')
-        composedMessage = 'Information from web service: Message: ' << msg.to_s if msg
-        Views.configuration.logger.error "Error occured while contacting VIEWS: " + composedMessage.to_s if composedMessage
+        composed_message = 'Information from web service: Message: ' << msg.to_s if msg
+        Views.configuration.logger.error 'Error occured while contacting VIEWS: ' + composed_message&.to_s if composed_message
         messages[i] = 'Unable to run VIEWS validation, error occured.  Contact System Administrator.'
-      when 'Spelling'   
+      when 'Spelling'
         suggestions = message.xpath('./WebMMDS:Suggestion', WebMMDS: 'WebMMDS')
-        composedMessage = 'Spelling Suggestions: For field: ' << field << ', Term: ' << term << ', Suggestions: ' 
+        composed_message = 'Spelling Suggestions: For field: ' << field << ', Term: ' << term << ', Suggestions: '
         # probably a better way to do this, just getting the suggestions in an array
         # so I can use join() on it
-        suggestionArr = []
+        suggestion_arr = []
         suggestions.each do |s|
-          suggestionArr << s.text
+          suggestion_arr << s.text
         end
-        composedMessage << suggestionArr.join(',')
-        messages[i] = composedMessage
-        Views.configuration.logger.info("composedMessage " + composedMessage&.to_s)
+        composed_message << suggestion_arr.join(',')
+        messages[i] = composed_message
+        Views.configuration.logger.info('composed_message ' + composed_message&.to_s)
       when 'RareWord'
         suggestions = message.xpath('./WebMMDS:Suggestion', WebMMDS: 'WebMMDS')
         msg = message.at_xpath('./WebMMDS:Message[@level=3]', WebMMDS: 'WebMMDS')
         # probably a better way to do this, just getting these in an array
         # so I can use join() on it
-        suggestionArr = []
+        suggestion_arr = []
         suggestions.each do |s|
-          suggestionArr << s.text
+          suggestion_arr << s.text
         end
-        composedMessage = 'Rare Word: For field: ' << field << ', Term: ' << term << ', Message: ' << msg << ', Suggestions: ' 
-        suggestionMessage = suggestionArr.join(',')
-        composedMessage << suggestionMessage
-        messages[i] = composedMessage
+        composed_message = 'Rare Word: For field: ' << field << ', Term: ' << term << ', Message: ' << msg << ', Suggestions: '
+        suggestion_message = suggestion_arr.join(',')
+        composed_message << suggestion_message
+        messages[i] = composed_message
       when 'Surveillance'
         msg = message.at_xpath('./WebMMDS:Message[@level=3]', WebMMDS: 'WebMMDS')
-        composedMessage = 'Surveillance Word: For field: ' << field << ', Term: ' << term << ', Message: ' << msg
-        messages[i] = composedMessage
-      else 
+        composed_message = 'Surveillance Word: For field: ' << field << ', Term: ' << term << ', Message: ' << msg
+        messages[i] = composed_message
       end
     end
-  rescue Exception => err
-    Views.configuration.logger.error "Error occured while contacting VIEWS: " + err.to_s
+  rescue StandardError => err
+    Views.configuration.logger.error 'Error occured while contacting VIEWS: ' + err.to_s
     messages[0] = 'Unable to run VIEWS validation, error occured.  Contact System Administrator.'
     raise
   end
-  
-  #handle error case
+
+  # handle error case
   # TODO:  Implement
 
   messages
--- spec/views_spec.rb
+++ spec/views_spec.rb
@@ -1,8 +1,8 @@
 # test for views_ws gem
-require_relative "../lib/views/views"
+require_relative '../lib/views/views'
 
 describe Views do
-  before :each do 
+  before :each do
     Views.configure do |config|
       config.log = true
       logger = Logger.new($stdout)
@@ -10,48 +10,44 @@ describe Views do
       config.logger = logger
     end
   end
-  
-  describe "#validate" do
-    
-    #TODO:  Uncomment to run
-    
-#    context "provide some valid and error-free data" do
-#      it "returns empty array" do
-#        expect(Views.validate(cause_of_death_line1: 'Heart Attack', cause_of_death_duration1: '1 week')).to be_empty
-#      end
-#    end
-#    
-#    context "provide some data with errors or warnings" do
-#      it "returns array with messages" do
-#        expect(Views.validate(cause_of_death_line1: 'Haert Attack', cause_of_death_duration1: '1 week').size).to eq(1) 
-#      end
-#    end
 
-    context "provide full data with no errors or warnings" do
-      it "returns empty array" do
-        expect(Views.validate(cause_of_death_line1: 'Heart Attack', 
-                                       cause_of_death_duration1: '1 week',
-                                       cause_of_death_line2: 'Heart Disease', 
-                                       cause_of_death_duration2: '2 years',
-                                       cause_of_death_line3: 'Bad Diet', 
-                                       cause_of_death_duration3: '30 years',
-                                       cause_of_death_line4: 'Fell', 
-                                       cause_of_death_duration4: '32 years',
-                                       actual_or_presumed_date_of_death: Date.parse('2017-03-02'),
-                                       date_of_injury: Date.parse('2017-03-01'),
-                                       time_of_injury: Time.parse('12:30'),
-                                       place_of_injury: 'home', 
-                                       description_of_injury_occurrence: 'fell',
-                                       transportation_injury_role: 'none',
-                                       sex: 'M',
-                                       date_of_birth: Date.parse('1959-03-02'),
-                                       did_tobacco_use_contribute_to_death: 'N',
-                                       was_an_autopsy_performed: 'N',
-                                       were_autopsy_findings_available: 'N',
-                                       manner_of_death: 'N',
-                                       injury_at_work: 'N')).to be_empty
+  describe '#validate' do
+    context 'provide some valid and error-free data' do
+      it 'returns empty array' do
+        expect(Views.validate(cause_of_death_line1: 'Heart Attack', cause_of_death_duration1: '1 week')).to be_empty
+      end
+    end
+
+    context 'provide some data with errors or warnings' do
+      it 'returns array with messages' do
+        expect(Views.validate(cause_of_death_line1: 'Haert Attack', cause_of_death_duration1: '1 week').size).to eq(1)
+      end
+    end
+
+    context 'provide full data with no errors or warnings' do
+      it 'returns empty array' do
+        expect(Views.validate(cause_of_death_line1: 'Heart Attack',
+                              cause_of_death_duration1: '1 week',
+                              cause_of_death_line2: 'Heart Disease',
+                              cause_of_death_duration2: '2 years',
+                              cause_of_death_line3: 'Bad Diet',
+                              cause_of_death_duration3: '30 years',
+                              cause_of_death_line4: 'Fell',
+                              cause_of_death_duration4: '32 years',
+                              actual_or_presumed_date_of_death: Date.parse('2017-03-02'),
+                              date_of_injury: Date.parse('2017-03-01'),
+                              time_of_injury: Time.parse('12:30').utc,
+                              place_of_injury: 'home',
+                              description_of_injury_occurrence: 'fell',
+                              transportation_injury_role: 'none',
+                              sex: 'M',
+                              date_of_birth: Date.parse('1959-03-02'),
+                              did_tobacco_use_contribute_to_death: 'N',
+                              was_an_autopsy_performed: 'N',
+                              were_autopsy_findings_available: 'N',
+                              manner_of_death: 'N',
+                              injury_at_work: 'N')).to be_empty
       end
     end
   end
-  
 end
